819c1f8a30c3bac7bf8e6904fc49a098253432c7
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-01-17 12:25:35 +0700

    Simpler version

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,15 @@
 class WelcomeController < ApplicationController
 	require 'json'
+	@@page = 1
   def index
-  	@apifile = open('http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30')
+  	@apifile = open("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page=#{'page'}")
   	@movie = ''
   	@apifile.each do |x|
   		@movie << x 
   	end
   	@movie = JSON.parse(@movie)
+  end
+  def next_page
+  	@@page+=1
   end
 end

76d4fb371cd886c9182342766485ff1defc6ae43
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-01-22 21:48:13 +0700

    overview add

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,25 @@
 class WelcomeController < ApplicationController
 	require 'json'
-	@@page = 1
+
   def index
-  	@apifile = open("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page=#{'page'}")
+    @page=1
+  	@apifile = open("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page=#{@page}")
   	@movie = ''
   	@apifile.each do |x|
   		@movie << x 
   	end
   	@movie = JSON.parse(@movie)
   end
-  def next_page
-  	@@page+=1
+
+  def show
+    @apifile = open("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page=#{@page}")
+    @movie = ''
+    @apifile.each do |x|
+      @movie << x 
+    end
+    @movie = JSON.parse(@movie)
+    @movie = @movie["results"]
+    num= params[:id].to_i
+    @movie = @movie[num]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	#require require '#{RAILS.root}/lib/movie.rb'
+
   get 'welcome/index'
-  root 'welcome#index'
+  resources :welcome
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
